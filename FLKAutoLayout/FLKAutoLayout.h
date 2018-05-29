addb371b02e732f82b87de4a931aad21f278f177
magicfeet <magicfeet55@gmail.com>
2018-05-28 20:09:32 -0600

    add framework

--- FLKAutoLayout/FLKAutoLayout.h
+++ FLKAutoLayout/FLKAutoLayout.h
@@ -1,2 +1,21 @@
+//
+//  FLKAutoLayout.h
+//  FLKAutoLayout
+//
+//  Created by Jeremy Smith on 5/28/18.
+//  Copyright © 2018 boost social. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FLKAutoLayout.
+FOUNDATION_EXPORT double FLKAutoLayoutVersionNumber;
+
+//! Project version string for FLKAutoLayout.
+FOUNDATION_EXPORT const unsigned char FLKAutoLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FLKAutoLayout/PublicHeader.h>
+
 #import "UIViewController+FLKAutoLayout.h"
 #import "UIView+FLKAutoLayout.h"
+
--- FLKAutoLayout/Private/UIView+FLKAutoLayoutPredicate.h
+++ FLKAutoLayout/Private/UIView+FLKAutoLayoutPredicate.h
@@ -1,4 +1,5 @@
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
 
 typedef struct {
     NSLayoutRelation relation;
